7016df521070e95eb0d711f046b271971c63da7f
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2021-11-21 20:46:59 +0100

    feat: ddl creation

---
+++ configs/DDL/dbns_create_nuclei.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS nuclei (
+    templateID VARCHAR (512),
+    host VARCHAR(512),
+    severity VARCHAR(64),
+    name TEXT,
+    tags VARCHAR(256),
+    matcher_name VARCHAR(512),
+    type VARCHAR(64),
+    matched VARCHAR(512),
+    ip VARCHAR(16),
+    first_discovered TIMESTAMP default now(),
+    last_change TIMESTAMP DEFAULT now(),
+    PRIMARY KEY(host, templateID)
+)
